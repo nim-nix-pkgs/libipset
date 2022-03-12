e9579b6d200465e8c346a7724e8161aa4308cd64
hugosenari <hugosenari@gmail.com>
2022-03-12 02:55:55 -0300

    chore: re index libipset

---
+++ 0_0_2/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''libipset wrapper for Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-libipset-0_0_2.flake = false;
+  inputs.src-libipset-0_0_2.owner = "ba0f3";
+  inputs.src-libipset-0_0_2.ref   = "refs/tags/0.0.2";
+  inputs.src-libipset-0_0_2.repo  = "libipset.nim";
+  inputs.src-libipset-0_0_2.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-libipset-0_0_2"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-libipset-0_0_2";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''libipset wrapper for Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-libipset-master.flake = false;
+  inputs.src-libipset-master.owner = "ba0f3";
+  inputs.src-libipset-master.ref   = "refs/heads/master";
+  inputs.src-libipset-master.repo  = "libipset.nim";
+  inputs.src-libipset-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-libipset-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-libipset-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
